e02ee53904bb1ac57929a4cf2d0d22b9ff8f8ecc
Josh-Stillman <joshstillman@gmail.com>
2017-11-01 23:15:58 -0400

    done

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+
+@@song_count = 0
+
+def self.song_count
+  @@song_count
+end
+
+
+attr_accessor :name, :songs
+
+def initialize(name)
+  @name = name
+  @songs = []
+end
+
+def add_song_by_name(title)
+  song = Song.new(title)
+  song.artist = self
+  @songs << song
+  @@song_count += 1
+end
+
+def add_song(song)
+  song.artist = self
+  @songs << song
+  @@song_count += 1
+end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+  @@post_count = 0
+
+  def self.post_count
+    @@post_count
+  end
+
+  attr_accessor :name
+  attr_reader :posts
+
+  def initialize(name)
+    @posts = []
+    @name = name
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+end
